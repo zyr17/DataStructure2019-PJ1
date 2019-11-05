95148f91543cc38e55bd2d4dd618f16a713faa54
zyr17 <jzjqz17@gmail.com>
2019-11-05 16:18:38 +0800

    datagen

--- maker.cpp
+++ maker.cpp
@@ -6,7 +6,11 @@ int a[20000];
 
 int main(int argc, char** argv ) {
 	n = atoi(argv[1]); 
-	k = atoi(argv[2]); 
+	k = atoi(argv[2]);
+    int seed = 0;
+    if (argc > 3)
+        seed = atoi(argv[3]);
+    srand(seed);
 	//cin>>n>>k;
 	for (int i = 1; i <= n; ++i) a[i] = i; 
 	for (int i = 1; i <= k; ++i) {
