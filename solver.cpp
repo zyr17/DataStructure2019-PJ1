8e2dae9c768c74364b6187bd6ab732cf15ee54ae
zyr17 <jzjqz17@gmail.com>
2019-11-05 14:05:03 +0800

    add comment

--- solver.cpp
+++ solver.cpp
@@ -1,18 +1,28 @@
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 int main() {
 	int n;
-	scanf("%d", &n);
+	//read array length
+	cin >> n;
 	for (int i = 100000; i --; ){
-		printf("1 3\n");
-		fflush(stdout);
+		//print x and y. 1 <= x, y <= n and x != y
+		cout << "1 3\n";
+		//flush stdout. you can also use { cout << endl; } to instead { cout << "\n"; cout << flush; }
+		cout << flush;
 		int t1;
-		scanf("%d", &t1);	
+		//get comparison result of a[x] and a[y]
+		cin >> t1;
 	}
-	printf("0 0\n2\n3 4\n3 5\n");
-	fflush(stdout);
+	//print 0 0 and flush to terminate ask
+	cout << "0 0" << endl;
+	//print swap times m and flush
+	cout << 2 << endl;
+	//print x, y to swap a[x] and a[y] and flush. 1 <= x, y <= n and x != y
+	cout << "3 4" << endl << "3 5" << endl;
 	int result;
-	scanf("%d", &result);
+	//!!!IMPORTANT!!! you should read the final score
+	//without this judger may get "reached EOF" error.
+	cin >> result;
 	return 0;
 }
